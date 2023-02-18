071b1c72436ba5cdb333168dcc6be1a638cda7f0
swami <amswam23@gmail.com>
2023-02-17 20:51:58 -0500

    Added new java file HelloWorld1.java

---
+++ HelloWorld1.java
@@ -0,0 +1,11 @@
+class HelloWorld1 {
+
+   public static void main ( String[] args ){
+      System.out.println("Step1");
+      int x=10;
+      System.out.println(x);
+
+   System.out.println("HelloWorld");
+}
+
+}
